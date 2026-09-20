2bb3cc810c9947569fb1623b4bb54655648b6821
minsik2e5 <minsik2e5@gmail.com>
2026-09-20 22:36:10 +0900

    Cache V13.10 simplified home

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'sumus-voca-v13.9.0-academy-ranking';
+const VERSION = 'sumus-voca-v13.10.0-simplified-home';
 const STATIC_CACHE = `${VERSION}-static`;
 const SHELL = [
   '/',
@@ -9,6 +9,7 @@ const SHELL = [
   '/v137.css',
   '/v138.css',
   '/v139.css',
+  '/v1310.css',
   '/app.js',
   '/signup-ui.js',
   '/teacher-enhancements.js',
