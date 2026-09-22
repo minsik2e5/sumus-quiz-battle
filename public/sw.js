6b35bf96d46e8afcdf85bc0a66a3d4f7ea0d0e96
minsik2e5 <minsik2e5@gmail.com>
2026-09-23 05:55:31 +0900

    V13.27 bump cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'sumus-voca-v13.26.0-focus-home-test-pwa';
+const VERSION = 'sumus-voca-v13.27.0-student-ux-finish';
 const STATIC_CACHE = `${VERSION}-static`;
 const SHELL = [
   '/',
@@ -21,6 +21,7 @@ const SHELL = [
   '/v1324.css',
   '/v1325.css',
   '/v1326.css',
+  '/v1327.css',
   '/app.js',
   '/signup-ui.js',
   '/teacher-enhancements.js',
